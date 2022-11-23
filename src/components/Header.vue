733f9f11fc9aed10b5b5bcf5c1802205d7ff6941
Mudivhi <Mudivhit8@gmail.com>
2022-11-23 14:14:02 +0200

    feat: access to chads smartcontract

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,22 +8,44 @@ export default defineComponent({
   name: 'Header',
   components: {},
   setup() {
-    return {
 
+    const chadContract = '0x1B84f3Db0EC59e1854f24d03596585F9740c9266';
+
+    return {
+        chadContract
     };
+
   },
   methods: {
 
     getAccountDetails:async () => {
-      
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
       const accounts = await provider.send('eth_requestAccounts', []);
       const chainId = await provider.send('eth_chainId', []);
       const account = accounts[0];
       const balance = await provider.getBalance(account);
 
       console.log(accounts, account, balance, chainId);
-    }
+    },
+
+    getContractDetails:async () => {
+
+      const ERC721_ABI = [
+      'function balanceOf(address) view returns (uint)',
+      'function name() view returns (string)',
+      'function tokenOfownerByIndex(address, index) view returns (uint)',
+      'function totalSupply() view returns (uint)',
+      'function tokenByIndex(index) view returns (uint)'
+      ];
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract('0x1B84f3Db0EC59e1854f24d03596585F9740c9266', ERC721_ABI, provider);
+      const totalSupply = await contract.totalSupply();
+      const name = await contract.name();
+      const balance = await contract.balanceOf('0xd04da43d4a6e3434399a495e6a0f5c426ecec3ea')
+
+      console.log(name, totalSupply , balance);
+      }
   }
 });
 </script>
@@ -31,15 +53,15 @@ export default defineComponent({
 q-header
   q-toolbar.row.q-py-sm.q-px-md.top-nav
     .row.full-length.items-center
-      .col-10
+      .col-2
         img.logo.q-mr-md(
           src='~assets/logo.png'
         )
+      .col-8
       .col-2
         q-btn(
-          id="ethereumButton"
           label="MetaMask Login"
-          @click="getAccountDetails"
+          @click="getContractDetails"
         )
 </template>
 <style scoped lang="sass">
