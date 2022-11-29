16eb2b7e2c52c30807cfc5e23e12957e355f11a7
Mudivhi <Mudivhit8@gmail.com>
2022-11-29 19:46:49 +0200

    wip: consolidated NFT data

--- src/components/NFTs.vue
+++ src/components/NFTs.vue
@@ -6,81 +6,13 @@ import {
 export default defineComponent({
   name: 'NFTs',
 
-  setup(){
-    const NFTDummies = [
-    {
-    'name': '#976',
-    'description': '',
-    'image': 'https://ipfs.io/ipfs/QmdGPQVFirRFB6f3YJckx7rivck67mmn5Qofw5xYowoZLd/976.png',
-    'dna': '5e4a2debaae8641c674a1509175fb54cdc84eb64',
-    'edition': 976,
-    'date': 1644596775821,
-      },
-    {
-    'name': '#970',
-    'description': '',
-    'image': 'ipfs://QmdGPQVFirRFB6f3YJckx7rivck67mmn5Qofw5xYowoZLd/440.png',
-    'dna': '5e4a2debaae8641c674a1509175fb54cdc84eb64',
-    'edition': 976,
-    'date': 1644596775821,
-      },
-      {
-    'name': '#979',
-    'description': '',
-    'image': 'ipfs://QmdGPQVFirRFB6f3YJckx7rivck67mmn5Qofw5xYowoZLd/976.png',
-    'dna': '5e4a2debaae8641c674a1509175fb54cdc84eb64',
-    'edition': 976,
-    'date': 1644596775821,
-      }
-    ];
-    return{
-      NFTDummies
-    }
-  },
-  methods: {
 
-    imageLink(url:string) : string {
-
-      const imageUrl = url.replace('ipfs://', 'https://ipfs.io/ipfs/');
-
-      return imageUrl;
-    }
-  }
 });
 </script>
 <template lang="pug">
-q-item.nft-data
-  .row.align-center.justify-center
-    q-card.q-ma-md(v-for='NFT in NFTDummies', bordered)
-      .row.flex-center
-        h4.card-header {{NFT.name}}
-      .row.flex-center
-        img.q-ma-md.logo(
-          :src= "imageLink(NFT.image)"
-        )
-      .col.text-center
-        .p.text-yellow CLAIMED : {{NFT.actions}}
-        .p.text-white AVAILABLE : {{NFT.file}}
-        .p.text-white LOCKED :  {{NFT.dataField1}}
 
-      .row.flex-center
-        q-card-actions(vertical)
-          q-btn(
-            label='Claim Now'
-            icon='star'
-          )
 
 </template>
 <style scoped lang="sass">
-.logo
-  height: 185px
-.nft-data
-  box-shadow: 0px 1px #ECECEC,
-  background-color:  black
-  width: 100%
-  height: 100%
-.card-header
-  color: white
-  text-align: center
-  vertical-align: middle
+
 </style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,14 +1,45 @@
 <script lang="ts">
-import NFTs from 'components/NFTs.vue'
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'IndexPage',
-  components: { NFTs },
+  components: {},
+  setup(){
+    const NFTDummies = [
+    {
+    'name': '#976',
+    'description': '',
+    'image': 'ipfs://QmdGPQVFirRFB6f3YJckx7rivck67mmn5Qofw5xYowoZLd/976.png',
+      },
+    {
+    'name': '#970',
+    'description': '',
+    'image': 'ipfs://QmdGPQVFirRFB6f3YJckx7rivck67mmn5Qofw5xYowoZLd/440.png',
+      },
+      {
+    'name': '#979',
+    'description': '',
+    'image': 'ipfs://QmdGPQVFirRFB6f3YJckx7rivck67mmn5Qofw5xYowoZLd/976.png',
+      }
+    ];
+    return{
+      NFTDummies
+    }
+  },
+  methods: {
+
+    imageLink(url:string) : string {
+
+      const imageUrl = url.replace('ipfs://', 'https://ipfs.io/ipfs/');
+
+      return imageUrl;
+    }
+  }
 });
 </script>
 <template lang="pug">
 q-page
+  //- Token Countdown and Claim All Area
   .row
     q-item.token-data
       q-item-section
@@ -23,9 +54,26 @@ q-page
           q-btn(
             label="Claim All"
           )
+  //- User NFTs
   .row
-    NFTs
-</template>
+    q-item.nft-data
+      .row.align-center.justify-center
+        q-card.q-ma-md(v-for='NFT in NFTDummies', bordered)
+          .row.flex-center
+            h4.card-header {{NFT.name}}
+          .row.flex-center
+            img.q-ma-md.logo(
+              :src= "imageLink(NFT.image)"
+            )
+          .col.text-center
+            .p.text-yellow CLAIMED : {{NFT.actions}}
+            .p.text-white AVAILABLE : {{NFT.file}}
+            .p.text-white LOCKED :  {{NFT.dataField1}}
 
-<style scoped lang="sass">
-</style>
+          .row.flex-center
+            q-card-actions(vertical)
+              q-btn(
+                label='Claim Now'
+                icon='star'
+              )
+</template>
