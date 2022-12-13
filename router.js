8ce133d0a1e358369ad64fb3d9197ae7b31c88bb
EPOHnomeL <lemonjonathanvorster@gmail.com>
2022-12-13 09:54:27 +0200

    Updated the Api for specific endpoints

--- router.js
+++ router.js
@@ -1,18 +1,25 @@
 var express = require("express");
 const { services } = require("./services");
 var router = express.Router();
+const cors = require("cors");
 
 router.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
+router.use(cors());
 
 router.get("/", function (req, res) {
   res.send("Welcome to Analytics API");
 });
 
+router.get("/usersNotValidated", async (req, res) => {
+  let data = await services.getUsersNotValidated();
+  res.send(data);
+});
+
 router.get("/:col", async function (req, res) {
-  let collection = await services.getCol(req.params.col)
+  let collection = await services.getCol(req.params.col);
   res.send(collection);
 });
 
--- services.js
+++ services.js
@@ -10,6 +10,26 @@ class services {
     }
     return res;
   }
+
+  static async getUsersNotValidated() {
+    let users = await getDb().collection("users").find({}).toArray();
+    let count = 0;
+    let msg = "None";
+    let usersNotValidated = [];
+    users.forEach((user) => {
+      if (user.verifications.kyc === "APPROVED") {
+        count++;
+      } else {
+        usersNotValidated.push(user.name);
+      }
+    });
+    if (count === users.length) {
+      this.allNotValidated = false;
+    }
+    msg = `${count}/${users.length}`;
+    const data = { msg, usersNotValidated };
+    return data;
+  }
 }
 
 module.exports = { services };
