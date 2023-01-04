c1df3c53a4d015ad06e0f4f780a8160a3b7b5caf
Mudivhi <Mudivhit8@gmail.com>
2023-01-04 12:43:49 +0200

    chore: Footer refactored

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -13,22 +13,18 @@ export default defineComponent({
 .q-py-md.bg-black
   .row.justify-center
     q-btn(
-      color="black"
       a href="https://discord.gg/gYrwj5hbtZ"
       target="_blank"
-      width="10px"
     )
       q-avatar(size='2.5rem')
         img(src='~/assets/dc.png')
     q-btn(
-      color="black"
       href="https://twitter.com/TheChadsClub"
       target="_blank"
     )
       q-avatar(size='2.5rem')
         img(src='~/assets/tw.png')
     q-btn(
-      color="black"
       href="https://opensea.io/collection/thechadsclub"
       target="_blank"
     )
@@ -39,7 +35,6 @@ export default defineComponent({
     q-btn(
         label='Terms & Conditions'
         unelevated
-        color="black"
         text-color="yellow"
         :ripple='false'
         :to='{ name: "terms-and-conditions" }'
@@ -49,4 +44,5 @@ export default defineComponent({
 <style lang="sass" scoped>
 .q-btn
   width: fit-content
+  background-color: black
 </style>
