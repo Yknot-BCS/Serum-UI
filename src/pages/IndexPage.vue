241ea57b8d12e532c5725e53341741030c968908
Dewald928 <dewald123@rocketmail.com>
2023-02-14 12:55:09 +0200

    hotfix: available

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -184,7 +184,8 @@ export default defineComponent({
     calculateAvailable(nft: any): string {
       if (nft.locked) {
         let locked = ethers.BigNumber.from(nft.locked as ethers.BigNumber);
-        if (locked.eq(0)) {
+        let available = ethers.BigNumber.from(nft.available as ethers.BigNumber);
+        if (locked.eq(0) && available.gt(0)) {
           if (nft.edition <= 777) {
             return this.calculateClaimable(52500);
           }
